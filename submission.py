5b1a4f39742f38e5101ef5da10bfcfbd89a9d0c1
gubaidulinvadim <gubaidulinvadim@gmail.com>
2023-09-20 10:18:27 +0200

    - added Q_s

--- submission.py
+++ submission.py
@@ -13,11 +13,12 @@ def write_tmp_submission_script_ccrt(job_name,
                                      Ib,
                                      Qp,
                                      min_value,
-                                     max_value, 
-                                     n_scan_points):
+                                     max_value,
+                                     n_scan_points,
+                                     Q_s):
     MOUNT_FOLDER = '/ccc/work/cont003/soleil/gubaiduv/delphi:/home/dockeruser/delphi'
     IMAGE_NAME = 'delphi'
-    SCRIPT_NAME = 'fbii_pyht_tracking/run_delphi.py'
+    SCRIPT_NAME = 'delphi/run_delphi.py'
     with open(job_name, "w") as f:
         f.write("#!/bin/bash\n")
         f.write("#MSUB -m work,scratch\n")
@@ -34,20 +35,21 @@ def write_tmp_submission_script_ccrt(job_name,
             "#MSUB -e /ccc/cont003/home/soleil/gubaiduv/{0:}.out\n".format(job_name))
         f.write('module purge\n')
         f.write(
-            "pcocc run --mount {0:} -I {1:} --entry-point -- python {2:} --filename {3:} --ID_state {4:} --sigma_z {5:} --plane {6:} --scan_type {7:} --current {8:}, --chromaticity {9:} --min_value {10:} --max_value {11:} --n_scan_points {12:}\n".format(MOUNT_FOLDER,
-                                                                                                                                                                                                                                                              IMAGE_NAME,
-                                                                                                                                                                                                                                                              SCRIPT_NAME,
-                                                                                                                                                                                                                                                              filename,
-                                                                                                                                                                                                                                                              ID_state,
-                                                                                                                                                                                                                                                              sigma_z,
-                                                                                                                                                                                                                                                              plane,
-                                                                                                                                                                                                                                                              scan_type,
-                                                                                                                                                                                                                                                              Ib,
-                                                                                                                                                                                                                                                              Qp,
-                                                                                                                                                                                                                                                              min_value,
-                                                                                                                                                                                                                                                              max_value,
-                                                                                                                                                                                                                                                              n_scan_points
-                                                                                                                                                                                                                                                              ))
+            "pcocc run --mount {0:} -I {1:} --entry-point -- python {2:} --filename {3:} --ID_state {4:} --sigma_z {5:} --plane {6:} --scan_type {7:} --current {8:} --chromaticity {9:} --min_value {10:} --max_value {11:} --n_scan_points {12:} --Q_s {13:}\n".format(MOUNT_FOLDER,
+                                                                                                                                                                                                                                                                         IMAGE_NAME,
+                                                                                                                                                                                                                                                                         SCRIPT_NAME,
+                                                                                                                                                                                                                                                                         filename,
+                                                                                                                                                                                                                                                                         ID_state,
+                                                                                                                                                                                                                                                                         sigma_z,
+                                                                                                                                                                                                                                                                         plane,
+                                                                                                                                                                                                                                                                         scan_type,
+                                                                                                                                                                                                                                                                         Ib,
+                                                                                                                                                                                                                                                                         Qp,
+                                                                                                                                                                                                                                                                         min_value,
+                                                                                                                                                                                                                                                                         max_value,
+                                                                                                                                                                                                                                                                         n_scan_points,
+                                                                                                                                                                                                                                                                         Q_s
+                                                                                                                                                                                                                                                                         ))
 
 
 def write_submission_script_slurm(job_name,
@@ -59,39 +61,42 @@ def write_submission_script_slurm(job_name,
                                   scan_type,
                                   Ib,
                                   Qp,
-                                  min_value, 
-                                  max_value, 
-                                  n_scan_points):
+                                  min_value,
+                                  max_value,
+                                  n_scan_points,
+                                  Q_s):
     MOUNT_FOLDER = '/lustre/scratch/sources/physmach/gubaidulin/delphi:/home/dockeruser/delphi'
     IMAGE_NAME = '/lustre/scratch/sources/physmach/gubaidulin/delphi.sif'
     SCRIPT_NAME = '/home/dockeruser/delphi/run_delphi.py'
     with open(job_name, "w") as f:
         f.write("#!/bin/bash\n")
-        f.write("#SBATCH --partition sumo\n")
+        f.write("#SBATCH --partition metis\n")
         f.write("#SBATCH -n 8\n")
         f.write("#SBATCH --time={:}\n".format(job_time))
         f.write('#SBATCH --export=ALL\n')
-        f.write('#SBATCH --gres=gpu:1\n')
         f.write("#SBATCH --mail-user='gubaidulinvadim@gmail.com'\n")
         f.write('#SBATCH --mail-type=begin,end,requeue\n')
         f.write(
             "#SBATCH --error=/home/sources/physmach/gubaidulin/err/{0:}.err\n".format(job_name))
+        f.write(
+            "#SBATCH --output=/home/sources/physmach/gubaidulin/out/{0:}.err\n".format(job_name))
         f.write('module load tools/singularity/current\n')
         f.write(
-            "singularity exec --no-home -B {0:} {1:} python {2:}  --filename {3:} --ID_state {4:} --sigma_z {5:} --plane {6:} --scan_type {7:} --current {8:} --chromaticity {9:} --min_value {10:} --max_value {11:} --n_scan_points {12:}\n".format(MOUNT_FOLDER,
-                                                                                                                                                                                            IMAGE_NAME,
-                                                                                                                                                                                            SCRIPT_NAME,
-                                                                                                                                                                                            filename,
-                                                                                                                                                                                            ID_state,
-                                                                                                                                                                                            sigma_z,
-                                                                                                                                                                                            plane,
-                                                                                                                                                                                            scan_type,
-                                                                                                                                                                                            Ib,
-                                                                                                                                                                                            Qp,
-                                                                                                                                                                                            min_value, 
-                                                                                                                                                                                            max_value, 
-                                                                                                                                                                                            n_scan_points
-                                                                                                                                                                                            ))
+            "singularity exec -e --no-home -B {0:} {1:} python {2:}  --filename {3:} --ID_state {4:} --sigma_z {5:} --plane {6:} --scan_type {7:} --current {8:} --chromaticity {9:} --min_value {10:} --max_value {11:} --n_scan_points {12:} --Q_s {13:}\n".format(MOUNT_FOLDER,
+                                                                                                                                                                                                                                                                     IMAGE_NAME,
+                                                                                                                                                                                                                                                                     SCRIPT_NAME,
+                                                                                                                                                                                                                                                                     filename,
+                                                                                                                                                                                                                                                                     ID_state,
+                                                                                                                                                                                                                                                                     sigma_z,
+                                                                                                                                                                                                                                                                     plane,
+                                                                                                                                                                                                                                                                     scan_type,
+                                                                                                                                                                                                                                                                     Ib,
+                                                                                                                                                                                                                                                                     Qp,
+                                                                                                                                                                                                                                                                     min_value,
+                                                                                                                                                                                                                                                                     max_value,
+                                                                                                                                                                                                                                                                     n_scan_points,
+                                                                                                                                                                                                                                                                     Q_s
+                                                                                                                                                                                                                                                                     ))
     return job_name
 
 
@@ -115,9 +120,12 @@ if __name__ == '__main__':
                                          args.current,
                                          args.chromaticity,
                                          args.min_value,
-                                         args.max_value, 
-                                         args.n_scan_points)
+                                         args.max_value,
+                                         args.n_scan_points,
+                                         args.Q_s)
         os.system('ccc_msub {:}'.format(args.job_name))
+        os.system('rm -rf {:}'.format(args.job_name))
+
     elif args.sub_mode == 'slurm':
         write_submission_script_slurm(args.job_name,
                                       args.job_time,
@@ -129,8 +137,10 @@ if __name__ == '__main__':
                                       args.current,
                                       args.chromaticity,
                                       args.min_value,
-                                      args.max_value, 
-                                      args.n_scan_points)
+                                      args.max_value,
+                                      args.n_scan_points,
+                                      args.Q_s)
         os.system('sbatch {:}'.format(args.job_name))
+        os.system('rm -rf {:}'.format(args.job_name))
     elif args.sub_mode == 'local':
         pass
